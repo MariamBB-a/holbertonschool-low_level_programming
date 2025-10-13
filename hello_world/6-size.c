700553415b0d203e600e65f708d9860148aa0c41
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-13 17:12:13 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - this sdesribes the main function of the code
- * Return: 0
- 	*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char a;
@@ -16,5 +18,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+
 	return (0);
 }
+
