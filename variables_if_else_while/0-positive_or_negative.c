0065a19c75b7c282a2b8f05a57dd0a4e110d8db7
root <mariam.backroush@gmail.com>
2025-10-14 19:05:36 +0000

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	int n;
 
-	srand(time (0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -30,5 +30,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 
-	return(0);
+	return (0);
 }
