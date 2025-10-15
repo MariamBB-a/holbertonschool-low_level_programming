b6652f35e7c8178c05a4387b50fca6359a194082
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-15 06:34:16 +0000

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 
-/* main: print letters
- * fuction: not p or e
- */
 
 int main(void)
 {
-	char = 'a'
-	while <= 'z'
-	{
-		if(char !='q' && char !='e')
-			putchar (char);
-		char ++;
-	}
-	
-	putchar ('\n')
-	
-	return '0';
+        char = 'a'
+        while <= 'z'
+        {
+                if (char ! = 'q' && char ! = 'e')
+                        putchar (char);
+                char++;
+        }
+        putchar ('\n')
+        return ('0');
 }
