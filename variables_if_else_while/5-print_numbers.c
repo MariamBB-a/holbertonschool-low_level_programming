8fef7dc15d814050d65032c14de12034a779bf92
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-15 07:46:50 +0000

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+#include <stdlib.h>
+
+/*
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * return: Always (success)
+ *
+ */
+
+int main(void)
+{ int num = 0;
+
+	while (num <10)
+	{
+		putchar (num + '0');
+
+		num++;
+
+		putchar ('/n')
+			
+			return (0);
+	}
