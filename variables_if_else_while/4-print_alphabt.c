ce6ac62b4c400e688d077c31b910db6aeb1d44bd
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-15 06:26:52 +0000

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* main: print letters
+ * fuction: not p or e
+ */
+
+int main(void)
+{
+	char = 'a'
+	while <= 'z'
+	{
+		if(char !='q' && char !='e')
+			putchar (char);
+		char ++;
+	}
+	
+	putchar ('\n')
+	
+	return '0';
+}
