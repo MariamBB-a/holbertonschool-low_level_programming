5cfcc87ed654a80b030b9484725a53600979c587
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-16 09:46:47 +0000

    alph

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - this is the function of the t
- * return: return 0
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
+ *
+ * Return: void
  */
-
 void print_alphabet(void)
 {
-char c;
-	for (c = 'a'; c <= 'z'; c++)
+	char c;
 
-		{
-			_putchar(c);
-		}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
+
