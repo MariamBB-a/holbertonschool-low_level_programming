22e4799c1a9950efb5ca226aeeda1777365d3719
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-17 16:07:37 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,20 @@
  */
 void print_alphabet_x10(void)
 {
-    
-    print_alphabet_x10();
+    int i = 0;
+    char c;
+
+    while (i < 10)
+    {
+        c = 'a';
+        while (c <= 'z')
+        {
+            _putchar(c);
+            c++;
+        }
+        _putchar('\n');
+        i++;
+    }
+
     return (0);
 }
