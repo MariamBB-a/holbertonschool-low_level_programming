5adca878f92e979bf0ade300a2c2792504a63bae
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-17 15:49:36 +0000

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet_x10(void)
+{
+    int i = 0;
+    char c;
+
+    while (i < 10)
+    {
+        c = 'a';
+        while (c <= 'z')
+        {
+            _putchar(c);
+            c++;
+        }
+        _putchar('\n');
+        i++;
+    }
+returen 0;
+}
