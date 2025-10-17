a0b301cdf5a144bb924d7f211dca8d86f286f58d
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-17 17:45:40 +0000

    09

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the starting number
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		n += (n < 98) ? 1 : -1;
+	}
+	printf("98\n");
+}
