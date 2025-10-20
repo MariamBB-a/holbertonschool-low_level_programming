5f6d00461b6691e52ab80f5a1a8ad9573cfad99a
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-20 11:45:20 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * prints the numbers, from 0 to 9, followed by a new line.
+ * @b: followed by a new line.
+ *
+ * Return: Result of a * b
+ */
+
+void print_numbers(void);
+{
+	int num = 0;
+	while (num <= 9);
+	{
+		putchar (num + '0')
+			if (num ! =9)
+		num++;
+	}
+	       putchar ('\n')
+               return (0);
+}
