577d51141f4c4500c3abacfa0297e5f37d57392a
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-20 11:04:35 +0000

    digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,10 @@
 
 
 /**
- * main - check the code
+ * * _isdigit - Checks if a character is a digit (0 through 9)
+ * @c: The character to be checked
  *
- * Return: Always 0.
+ * Return: 1 if c is a digit, 0 otherwise
  */
 
 int _isdigit(int c)
