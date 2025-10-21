a46d2f07f414522ed1ce9992713db48171eb5b7d
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-21 18:22:13 +0000

    11

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio>
+/**
+ * main - entry point
+ * description: prints the largest factor of a number.
+ * return: always 0
+ */
+int main(void)
+{
+	long n = 612852475143
+	long factor = 2;
+
+	while (factor * factor <= n)
+	{
+		if  n % factor == 0)
+		{
+			n /= factor;
+		}
+		else
+		{
+			factor++;
+		}
+	}
+	printf("%ld\n", n);
+	return (0);
+}
