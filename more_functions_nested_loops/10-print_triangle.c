a3d155a6878578507df8dde43888910d6a93abe6
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-21 10:21:47 +0000

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,19 @@
  * description: only use _putchar function to print
  * return: print space or new line.
  */
-void print_triangle(int size);
+void print_triangle(int size)
 {
-	int i; j;
+	int i, j;
 	
 	if (size <= 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
 	else
 	{
-		for(i = 0; i <= size; i++)
+		for(i = 1; i <= size; i++)
 		{
-			for(j =0; j<= size; j++)
+			for(j = 1; j<= size; j++)
 			{
 				_putchar('#');
 			}
