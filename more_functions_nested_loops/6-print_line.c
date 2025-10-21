1194aa3871d119da507a10226b18a62c74526209
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-21 07:37:39 +0000

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * main - draws a straight line in the terminal
+ * description: can only use _putchar function to print
+ * return: 0 if print new line
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		int i;
+		for( i = 0; i <= n; i++)
+		{
+			_putchar('-');
+			_putchar('\n');
+		}
+	}
+}
