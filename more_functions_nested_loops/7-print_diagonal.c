d1efd383a661cd47c38faa1ec61c423e1af6c45d
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-21 08:20:23 +0000

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,6 +3,7 @@
 /**
  * print_line - draws a diagonal line in the terminal
  * @n: number of times the character _ should be printed
+ *
  * Prints a diagonal line on the terminal by printing the character \
  * n times, each on a new line and preceded by spaces to form a diagonal.
  * If n is 0 or less, prints only a newline.
