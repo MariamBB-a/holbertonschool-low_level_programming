6eda32431f26ac5fe8c9e63763e35d7b00374d8c
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-21 10:24:29 +0000

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,16 @@
 void print_triangle(int size)
 {
 	int i, j;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for(i = 1; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for(j = 1; j<= size; j++)
+			for (j = 1; j <= size; j++)
 			{
 				_putchar('#');
 			}
