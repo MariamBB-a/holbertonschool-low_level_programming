ae2c5dacd6cd56f7f6c52a6384a36862e2c6145b
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-21 08:46:19 +0000

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,6 @@ void print_square(int size)
 	int i, j;
 
 	if (size <= 0)
-
 	{
 		_putchar('\n');
 	}
@@ -23,8 +22,10 @@ void print_square(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
+			{
 				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
