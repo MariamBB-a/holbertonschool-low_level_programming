181214475e6fa83e5daac31c400c78331627a139
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-22 12:27:14 +0000

    1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -10,11 +10,7 @@ void swap_int(int *a, int *b)
 {
     int temp;
 
-    printf("Before swap: a = %d, b = %d\n", *a, *b);
-
     temp = *a;
     *a = *b;
     *b = temp;
-
-    printf("After swap: a = %d, b = %d\n", *a, *b);
 }
