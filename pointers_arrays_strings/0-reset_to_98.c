e3d764f71efbcba46e6d345f54e80903fbd3120e
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-22 11:49:26 +0000

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * main - peint entery point
- * print 98 
+ * reset_to_98 - takes a pointer to an int and updates
+ *               the value it points to to 98
+ * @n: pointer to an int 
  *
  * return: always success
  */
 
 void reset_to_98(int *n);
 {
-
 	*n = 98;
-	
-	return (0);
 }
