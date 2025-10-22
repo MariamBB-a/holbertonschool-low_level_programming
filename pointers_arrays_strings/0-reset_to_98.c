628a7810ea2284e8c8792273fb3d3c438d303839
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-22 11:37:13 +0000

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - peint entery point
+ * print 98  The character to print
+ *
+ * return: always success
+ */
+
+void reset_to_98(int *n);
+{
+	int *n;
+
+	*n = 98;
+	
+	return (0);
+}
