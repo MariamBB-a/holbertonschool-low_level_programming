8441b373abe9c798b6e47d06e8a288bf1ed94a94
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-23 08:18:12 +0000

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ *
+ */
+
+void _puts(char *str)
+{
+	char str[] = "stdout";
+		printf( "%s", str);
+}
