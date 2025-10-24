68c839728676b452f5c146eb31bcf8df778bc35d
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-24 10:12:05 +0000

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_rev - prints a string in reverse followed by a new line
- * @s: pointer to the string to be reversed
+ * @s: pointer to the string
  */
 void print_rev(char *s)
 {
