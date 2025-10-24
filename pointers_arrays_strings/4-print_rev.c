8415874bc9a295e60b45641f159d419392dd86e2
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-24 08:38:13 +0000

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,6 @@
  * return: success
  */
 void print_rev(char *s);
-
 {
     int length = 0;
     int i;
@@ -17,8 +16,10 @@ void print_rev(char *s);
         length++;
     }
      for (i = length - 1; i >= 0; i--)
+
     {
         _putchar(s[i]);
     }
+
     _putchar('\n');
 }
