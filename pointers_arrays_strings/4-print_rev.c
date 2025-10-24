617c8830d0a72b047c8750ee511f055e4a335723
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-24 10:08:04 +0000

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
+    int length = 0;
 
-    while (s[len] != '\0')
-        len++;
+    while (s[length] != '\0')
+        length++;
 
-    for (len = len - 1; len >= 0; len--)
-        putchar(s[len]);
+    while (length--)
+        _putchar(s[length]);
 
-   putchar('\n');
+    _putchar('\n');
 }
