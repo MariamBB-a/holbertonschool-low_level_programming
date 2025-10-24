5b0d0bf5b75030713b84e77d038578344d1d08f6
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-24 10:37:01 +0000

    5

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,17 +12,13 @@ int _atoi(char *s)
 	int result = 0;
 	int sign = 1;
 
-	/* skip non-digit characters, handle signs */
 	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] == '+')
-			;
 		i++;
 	}
 
-	/* convert numeric characters */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		result = result * 10 + (s[i] - '0');
