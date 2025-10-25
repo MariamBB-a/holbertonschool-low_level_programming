03dbe1cf44d0d8f6b98da5e8388254e31d029255
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-25 19:23:41 +0000

    5

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: pointer to the string
+ *
+ * Return: pointer to the resulting string
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0'};
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] = str[i + 1] - 32;
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
