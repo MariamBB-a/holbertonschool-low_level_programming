941538cc87cee451c4f532143a8c44a421febd94
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-27 15:25:18 +0000

    reverse s b

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,15 +2,16 @@
 
 /**
  * _memset() fill with bytes
- * @n filled by
- * @s memory area copy from
- * @b memory value
+ * @n number of bytes
+ * @s pointer to fill
+ * @b constants beyts
  *
  * return to pointer
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
