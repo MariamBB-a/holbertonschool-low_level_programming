8cf1533827fb5933ee11292a18bc5505e092aefb
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-27 15:26:37 +0000

    reverse s b

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memset() fill with bytes
- * @n number of bytes
- * @s pointer to fill
- * @b constants beyts
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area to fill
+ * @b: the constant byte to fill the memory with
+ * @n: number of bytes to fill
  *
- * return to pointer
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
