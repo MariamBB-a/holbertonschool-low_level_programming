23242c82e8751e73fd42d4c9a2e684e5afaf1714
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-28 10:58:10 +0000

    strings

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stddef.h>
 
-/*
+/**
  *_strpbrk - searches a string for any of a set of bytes
  * @s: the string to be searched
  * @accept: the string containing the bytes to look for
