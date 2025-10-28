a9af1b68855ef5b57904e1fdbf4e963670309e4e
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-28 10:42:26 +0000

    strings

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ *main: find a character in two strings
+ *s: is a string yout are looking through
+ *accept: is another string you are looking through
+ *results: if nothing (NULL)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		for (char *a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
