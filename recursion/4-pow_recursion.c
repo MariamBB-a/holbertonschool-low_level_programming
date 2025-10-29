087301c379a980e45c2b4a8be8dba93a114b2eb9
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-29 10:46:16 +0000

    vale x,y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
+ * pow-recrusion - returns the value of x raised to the power of y
+ * @x base number
+ * @y expont
  *
- * pow-recrusion that returns the value of x raised to the power of y
- * x base number
- * y expont
- *
- * return: x,y,less than 0
+ * return: The result of x raised to y, or -1 if y is less than 0
  */
 int _pow_recursion(int x, int y)
 {
