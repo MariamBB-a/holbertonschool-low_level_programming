805611ae633838ab3898016c33fcef2847d26d45
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-29 09:32:50 +0000

    rev

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ *
+ * Return: Nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_rev-recursion(s + 1);
+}
