7d55d2965d8afc8dba14115fa3e048c0b0dacdd2
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-29 10:04:27 +0000

    count recrusion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string to measure
  *
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to measure.
- *
- * Return: Length of the string.
+ * Return: The length of the string
  */
-int _strlen_recursion(char *s)
+int _strlen_recursion (char *s)
 {
-	if (*s == '\0')
-	{
-		return(0);
-	}
-	return (1 + _strlen_recursion (s + 1));
+    if (*s == '\0')
+        return (0);
+
+    return (1 + _strlen_recursion(s + 1));
 }
