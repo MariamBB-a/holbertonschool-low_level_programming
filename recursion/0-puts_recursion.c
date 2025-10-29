83e3f7c0b5c2c79a6833ca316e761b7e2b6d5643
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-29 09:08:19 +0000

    first recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: pointer to the string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar ('\n');
+		return (0);
+	}
+	putchar (*s);
+	_put_recursion (s + 1);
+}
