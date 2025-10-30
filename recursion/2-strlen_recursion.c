77f3ae08da34c84f56a97e8fd5cfdeac2c30ebe5
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-10-30 10:10:46 +0000

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,10 +6,9 @@
  *
  * Return: The length of the string
  */
-int _strlen_recursion (char *s)
+int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
-
+	if (*s == '\0')
+	    return (0);
     return (1 + _strlen_recursion(s + 1));
 }
