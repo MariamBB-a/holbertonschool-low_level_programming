958c40d5053e6f529e090fb391fd860c98625e64
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-02 14:33:29 +0000

    0

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,33 +1,28 @@
 #include "main.h"
 
 /**
- * is_palindrome - checks if a string is a palindrome (recursively)
+ * is_palindrome - returns 1 if a string is a palindrome, 0 otherwise
  * @s: the string to check
  *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	if (!*s)
+	int len = 0;
+
+	if (*s == '\0')
 		return (1);
 
-	if (*(s + 1) == '\0')
+	for (; s[len] != '\0'; len++)
+		;
+
+	if (*s != s[len - 1])
+		return (0);
+
+	if (len <= 2)
 		return (1);
 
-	if (*(s + 1) && *(s + 2) == '\0')
-	{
-		if (*s == *(s + 1))
-			return (1);
-		else
-			return (0);
-	}
-	if (*s == *(s + (_strlen_recursion(s) - 1)))
-	{
-		char end = *(s + (_strlen_recursion(s) - 1));
-		*(s + (_strlen_recursion(s) - 1)) = '\0';
-		if (*(s + 1) == '\0')
-			return (1);
-		return (is_palindrome(s + 1));
-	}
-	return (0);
+	s[len - 1] = '\0';
+
+	return (is_palindrome(s + 1));
 }
