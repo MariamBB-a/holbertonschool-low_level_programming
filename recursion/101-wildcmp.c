9ed4cb234c90852a1a87ff73dd2ba40fa65f578b
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-02 16:30:00 +0000

    3

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,29 +1,25 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if they can be considered
- * identical, otherwise returns 0
- * @s1: the first string
- * @s2: the second string (can contain the special character '*')
+ * wildcmp - compares two strings considering '*' as a wildcard
+ * @s1: first string
+ * @s2: second string (can contain '*')
  *
- * Return: 1 if strings are identical, 0 otherwise
+ * Return: 1 if strings can be considered identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
 	if (*s2 == '*')
 	{
-		while (*(s2 + 1) == '*')
-			s2++;
-
-		if (*(s2 + 1) == '\0')
+		if (wildcmp(s1, s2 + 1))
 			return (1);
 
 		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 			return (1);
-		return (wildcmp(s1, s2 + 1));
+		return (0);
 	}
 
 	if (*s1 == *s2)
