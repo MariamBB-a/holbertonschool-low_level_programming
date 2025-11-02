65b55f95ecaf3d71cb2aa75ff9f0fe06de821218
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-02 09:58:21 +0000

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include <math.h>
+
+/**
+ * is_prime_number - checks if an integer is a prime number
+ * @n: the number to check
+ *
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+    int i;
+
+    if (n <= 1)
+        return (0);
+    if (n == 2)
+        return (1);
+    if (n % 2 == 0)
+        return (0);
+
+    for (i = 3; i <= sqrt(n); i += 2)
+    {
+        if (n % i == 0)
+            return (0);
+    }
+    return (1);
+}
