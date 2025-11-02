cfe58c507434e41ffae6482ab06cad8e49ab0317
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-02 16:41:17 +0000

    423

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,28 +3,25 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to find the square root of
- *
- * Return: natural square root, -1 if none
+ * Return: natural square root, or -1 if none exists
  */
 int _sqrt_recursion(int n)
 {
-	int candidate;
+	int guess;
 
 	if (n < 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
 
-	candidate = 1;
+	guess = 1;
 
-	if (candidate * candidate > n)
-		return (-1);
-	if (candidate * candidate == n)
-		return (candidate);
+	while (guess * guess < n)
+	{
+		guess++;
+	}
 
-	candidate = _sqrt_recursion(n - 1);
-	if (candidate * candidate == n)
-		return (candidate);
-
+	if (guess * guess == n)
+		return (guess);
 	return (-1);
 }
