8d77af3bf05023c36ba2842f81ca2f8cc900a2a9
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-04 10:34:24 +0000

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,17 +10,19 @@
  * Return: Nothing.
  */
 char *create_array(unsigned int size, char c)
-	unsigned int i;
-	char *arr;
 {
-	
-	if (size == 0)
-	return (NULL);
+	char *arr;
+	unsigned int i;
 
-	arr = malloc( size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(size * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
-		for (i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 		arr[i] = c;
-		return arr;
+
+	return (arr);
 }
