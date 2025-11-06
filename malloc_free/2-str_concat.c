e7d0b92245e3cc05ac4cc26d3d4e1ba2026c7ca0
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-06 07:38:39 +0000

    two strings,length

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,34 +11,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *new_str;
-    int i, j;
-    int len1 = 0, len2 = 0;
+	char *new_str;
+	int i, j;
+	int len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    /* Find length of s1 */
-    while (s1[len1] != '\0')
-        len1++;
+	/* Find length of s1 */
+	while (s1[len1] != '\0')
+		len1++;
 
-    /* Find length of s2 */
-    while (s2[len2] != '\0')
-        len2++;
+	/* Find length of s2 */
+	while (s2[len2] != '\0')
+		len2++;
 
-    new_str = malloc((len1 + len2 + 1) * sizeof(char));
-    if (new_str == NULL)
-        return (NULL);
+	new_str = malloc((len1 + len2 + 1) * sizeof(char));
+	if (new_str == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        new_str[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		new_str[i] = s1[i];
 
-    for (j = 0; j < len2; j++, i++)
-        new_str[i] = s2[j];
+	for (j = 0; j < len2; j++, i++)
+		new_str[i] = s2[j];
 
-    new_str[i] = '\0';
+	new_str[i] = '\0';
 
-    return (new_str);
+	return (new_str);
 }
