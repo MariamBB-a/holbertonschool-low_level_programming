196cee17826d7f2b689da1b16a166809313812cc
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-06 07:37:02 +0000

    two strings,length

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * str_concat - Concatenates two strings.
@@ -21,17 +20,22 @@ char *str_concat(char *s1, char *s2)
     if (s2 == NULL)
         s2 = "";
 
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+    /* Find length of s1 */
+    while (s1[len1] != '\0')
+        len1++;
+
+    /* Find length of s2 */
+    while (s2[len2] != '\0')
+        len2++;
 
     new_str = malloc((len1 + len2 + 1) * sizeof(char));
     if (new_str == NULL)
         return (NULL);
 
-    for (i = 0; s1[i] != '\0'; i++)
+    for (i = 0; i < len1; i++)
         new_str[i] = s1[i];
 
-    for (j = 0; s2[j] != '\0'; j++, i++)
+    for (j = 0; j < len2; j++, i++)
         new_str[i] = s2[j];
 
     new_str[i] = '\0';
