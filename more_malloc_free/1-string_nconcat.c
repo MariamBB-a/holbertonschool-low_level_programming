9fe9baab6995b35047b397221160af2a3e9eaa39
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-08 18:57:54 +0000

    2 srings allocated

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,37 +11,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *new_str;
-    int i, j;
-    int len1 = 0, len2 = 0;
-    int s2_copy_len;
+	char *new_str;
+	int i, j;
+	int len1 = 0, len2 = 0;
+	int s2_copy_len;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	/* Calculate lengths of s1 and s2 */
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    if (n >= (unsigned int)len2)
-        s2_copy_len = len2;
-    else
-        s2_copy_len = n;
+	/* Determine number of characters to copy from s2 */
+	if (n >= (unsigned int)len2)
+		s2_copy_len = len2;
+	else
+		s2_copy_len = n;
 
-    new_str = malloc(sizeof(char) * (len1 + s2_copy_len + 1));
-    if (new_str == NULL)
-        return (NULL);
+	/* Allocate memory for new string (+1 for null terminator) */
+	new_str = malloc(sizeof(char) * (len1 + s2_copy_len + 1));
+	if (new_str == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        new_str[i] = s1[i];
+	/* Copy s1 */
+	for (i = 0; i < len1; i++)
+		new_str[i] = s1[i];
 
-    for (j = 0; j < s2_copy_len; j++)
-        new_str[i + j] = s2[j];
+	/* Copy first s2_copy_len characters from s2 */
+	for (j = 0; j < s2_copy_len; j++)
+		new_str[i + j] = s2[j];
 
-    new_str[i + j] = '\0';
+	/* Null-terminate */
+	new_str[i + j] = '\0';
 
-    return (new_str);
+	return (new_str);
 }
