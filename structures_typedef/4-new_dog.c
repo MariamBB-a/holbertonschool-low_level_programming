df3d8cf61fe91844c2ffc3e317b3c92670e11364
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-09 10:32:44 +0000

    allocate

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -13,27 +12,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int i, name_len, owner_len;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	d->name = malloc(strlen(name) + 1);
+	for (name_len = 0; name[name_len] != '\0'; name_len++)
+		;
+
+	d->name = malloc(name_len + 1);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->name, name);
+	for (i = 0; i <= name_len; i++)
+		d->name[i] = name[i];
 
-	d->owner = malloc(strlen(owner) + 1);
+	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
+		;
+
+	d->owner = malloc(owner_len + 1);
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->owner, owner);
+	for (i = 0; i <= owner_len; i++)
+		d->owner[i] = owner[i];
 
 	d->age = age;
 
