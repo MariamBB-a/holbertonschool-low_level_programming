21c538cb4e7eb01e007f01db5aa2161ac7bbdae4
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-09 08:54:33 +0000

    01

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,16 +2,14 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
+ * @d: pointer to struct dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
- *
- * Description: Assigns the provided values to the members of struct dog.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != 0)
+    if (d != 0)
     {
         d->name = name;
         d->age = age;
