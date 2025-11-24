b2ce73bebbf079e3c7aa32c843d27df1f3ac17f4
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-11-24 09:11:26 +0000

    more

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stddef.h>
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
