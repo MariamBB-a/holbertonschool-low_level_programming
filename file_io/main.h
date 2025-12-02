1b3e9a3287a8d12d653ce18c3ee0d0cf0a986ce7
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-02 11:15:27 +0000

    prototype

---
+++ file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Existing prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Add other function prototypes here */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+
+#endif /* LISTS_H */
+
