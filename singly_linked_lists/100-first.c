fba129fc9c880b1f4889a14f68ea21f5bb884d0d
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-04 07:42:29 +0000

    write a sentence

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -2,8 +2,11 @@
 
 void __attribute__((constructor)) print_before_main(void);
 
+/**
+ * print_before_main - prints a message before main is executed
+ */
 void print_before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
