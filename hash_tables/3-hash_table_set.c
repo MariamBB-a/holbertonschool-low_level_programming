b36e5ccf0a4956c2dd361e93b09975dc7686c0b9
‘MariamBB-a’ <mariam.backroush@gmail.com>
2025-12-08 10:48:25 +0000

    so many free nodes

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to a hash table
+ * @ht: The hash table
+ * @key: The key (cannot be an empty string)
+ * @value: The value associated with the key
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node, *temp;
+
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
